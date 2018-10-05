4cc4f6e6c14a33e29d737db8b19ea726acedfb7a
DaffaTakarai <gacktioto@gmail.com>
2018-10-05 18:31:34 +0700

    Mengertilah

---
+++ src/kamu.kt
@@ -0,0 +1,48 @@
+package KamuCantik
+/*
+ada suatu hal yang selalu aku ingat tantang dirimu
+-DaffaTakarai
+*/
+
+fun main (args: Array<String>){
+	val data = mapOf('B' to "Bagiku Kamu Adlah Bidadari Tanpa Sayap",'Y' to "Yang Selalu Membuatku Terpikiranmu",'S' to "Suatu Saat Aku Pasti Bisa Bersama mu")
+	for(getKey in data.keys){
+	println(data[getKey
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	])
+	}
+}
